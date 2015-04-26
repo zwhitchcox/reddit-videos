5775351d8d5cb37ff6f3a15d1820dac379238463
Zane Hitchcox <zwhitchcox@gmail.com>
2015-04-26 09:32:11 -0400

    update

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ app.controller('Ctrl', ['$scope','$resource','$http', function($scope,$resource,
     player = new YT.Player('player', {
       height: '390',
       width: '640',
-      playerVars: { 'autoplay': 1},
+      playerVars: { 'autoplay': 1, 'playlist':$scope.vids},
       events: {
         'onReady': onPlayerReady,
         'onStateChange': onPlayerStateChange
@@ -37,7 +37,6 @@ app.controller('Ctrl', ['$scope','$resource','$http', function($scope,$resource,
 
     // 4. The API will call this function when the video player is ready.
     function onPlayerReady(event) {
-      player.cuePlaylist($scope.vids)
       event.target.playVideo();
     }
 
