c9d5fd082017c2f28ef890709934a864ce21469c
Zane Hitchcox <zwhitchcox@gmail.com>
2015-04-26 09:36:57 -0400

    update

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ app.controller('Ctrl', ['$scope','$resource','$http', function($scope,$resource,
     player = new YT.Player('player', {
       height: '390',
       width: '640',
-      playerVars: { 'autoplay': 1},
+      playerVars: { 'autoplay': 1, 'playlist':$scope.vids},
       events: {
         'onReady': onPlayerReady,
         'onStateChange': onPlayerStateChange
@@ -37,7 +37,6 @@ app.controller('Ctrl', ['$scope','$resource','$http', function($scope,$resource,
 
     // 4. The API will call this function when the video player is ready.
     function onPlayerReady(event) {
-      player.cuePlaylist($scope.vids)
       event.target.playVideo();
     }
 
@@ -46,7 +45,10 @@ app.controller('Ctrl', ['$scope','$resource','$http', function($scope,$resource,
     //    the player should play for six seconds and then stop.
     var done = false;
     function onPlayerStateChange(event) {
-
+      if (event.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;
+      }
     }
     function stopVideo() {
       player.stopVideo();
