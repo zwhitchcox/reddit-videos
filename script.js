5116a076e2de7533e50433b14c665c3f58b73009
Zane Hitchcox <zwhitchcox@gmail.com>
2015-04-26 09:46:29 -0400

    update

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ app.controller('Ctrl', ['$scope','$resource','$http', function($scope,$resource,
     var done = false;
     function onPlayerStateChange(event) {
       if (event.data == YT.PlayerState.PLAYING && !done) {
-        setTimeout(event.target.playVideo(), 6000);
+        setTimeout(stopVideo, 6000);
         done = true;
       }
     }
